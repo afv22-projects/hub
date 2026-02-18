ef3dc116356a3fad57bcb0f73f942facc8fbdc29
Andrew Vagliano <andrew.vagliano1@gmail.com>
2026-02-18 18:47:58 -0500

    chore: clean up db init file

--- db/__init__.py
+++ db/__init__.py
@@ -1,19 +1,14 @@
 import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-from sqlalchemy.orm import DeclarativeBase
+DB_URI = os.environ.get("DB_URI", "sqlite:///./app.db")
+engine = create_engine(DB_URI, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 class Base(DeclarativeBase): ...
-
-
-DB_URI = os.environ.get("DB_URI", "sqlite:///./app.db")
-
-engine = create_engine(DB_URI, connect_args={"check_same_thread": False})
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def init_db():
