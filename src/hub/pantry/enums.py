af3c1e214be65d6d8ccf286b7d5561745bdf5898
Andrew Vagliano <andrew.vagliano1@gmail.com>
2026-03-25 17:47:34 -0400

    refactory(hub): simplify pantry enums

--- src/hub/pantry/enums.py
+++ src/hub/pantry/enums.py
@@ -1,6 +1,16 @@
 from enum import Enum
 
 
+class ConsumableCategory(str, Enum):
+    """Categories for organizing household consumables."""
+
+    CLEANING = "cleaning"
+    PAPER_PRODUCTS = "paper_products"
+    PERSONAL_CARE = "personal_care"
+    KITCHEN_SUPPLIES = "kitchen_supplies"
+    BATHROOM_SUPPLIES = "bathroom_supplies"
+    OTHER = "other"
+
 class IngredientCategory(str, Enum):
     """Categories for organizing pantry ingredients."""
 
--- src/hub/pantry/enums/__init__.py
+++
@@ -1,7 +0,0 @@
-from hub.pantry.enums.consumable_category import ConsumableCategory
-from hub.pantry.enums.ingredient_category import IngredientCategory
-
-__all__ = [
-    "ConsumableCategory",
-    "IngredientCategory",
-]
